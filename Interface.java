ed383acabe038609a880963ffb83e1d49f1fb767
Ethan Albright <mrethan717@gmail.com>
2025-06-30 17:15:11 -0400

    Classes

---
+++ Interface.java
@@ -0,0 +1,19 @@
+public class Interface {
+
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public Interface(){
+
+    }
+
+
+
+
+
+
+
+}
