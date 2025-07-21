3d80e7c26bee18ccdf1a320fcf49dbec78feab23
RyanFDing <usatu2001@gmail.com>
2025-07-21 10:35:57 -0400

    broke

--- MiniMax.java
+++ MiniMax.java
@@ -6,7 +6,6 @@ public class MiniMax {
     private int x_wins;
     private int o_wins;
     private float[][] ratio = new float[3][3];
-    private int [] bestMove = new int[4];
 
     public MiniMax(){
         this.x_wins = 0;
@@ -18,9 +17,6 @@ public class MiniMax {
         }
     }
 
-    public int[] getBestMove() {
-        return bestMove;
-    }
     public float[][] eval(BigBoard board){
         
         SmallBoard [][] boards = board.getBoards();
@@ -31,13 +27,13 @@ public class MiniMax {
                 char [][] grid = sb.getGrid();
                 //col1
 
-                for (int c = 0; c < 3; c++){
-                    if (grid[c][0] != ' ' || grid[c][1] != ' ' || grid[c][2] != ' '){
-                        if (colWin(grid[c][0], 0, grid)!= ' '){ //if column has possible win
-                            if (colWin(grid[c][0], 0, grid) == 'X'){ // if the column has possible win for X
+                for (int c = 0; c < 3; c++){ //c is column number
+                    if (grid[0][c] != ' '){ //grid row c, col 0
+                        if (colWin(grid[0][c], 0, grid)!= ' '){ //if column has possible win
+                            if (colWin(grid[0][c], 0, grid) == 'X'){ // if the column has possible win for X
                                 x_wins++;
                                 } 
-                            else if (colWin(grid[c][0], 0, grid) == 'O'){ // if column has possible win for O
+                            else if (colWin(grid[0][c], 0, grid) == 'O'){ // if column has possible win for O
                                 o_wins++;
                             }
                         }     
@@ -47,12 +43,12 @@ public class MiniMax {
                 
                 
                 for (int c = 0; c < 3; c++){
-                    if (grid[c][0] != ' ' || grid[c][1] != ' ' || grid[c][2] != ' '){
-                        if (colWin(grid[c][1], 1, grid)!= ' '){ //if column has possible win
-                            if (colWin(grid[c][1], 1, grid) == 'X'){ // if the column has possible win for X
+                    if (grid[1][c] != ' '){
+                        if (colWin(grid[1][c], 1, grid)!= ' '){ //if column has possible win
+                            if (colWin(grid[1][c], 1, grid) == 'X'){ // if the column has possible win for X
                                 x_wins++;
                                 } 
-                            else if (colWin(grid[c][1], 1, grid) == 'O'){ // if column has possible win for O
+                            else if (colWin(grid[1][c], 1, grid) == 'O'){ // if column has possible win for O
                                 o_wins++;
                             }
                         }     
@@ -61,12 +57,12 @@ public class MiniMax {
                 }
                 
                 for (int c = 0; c < 3; c++){
-                    if (grid[c][0] != ' ' || grid[c][1] != ' ' || grid[c][2] != ' '){
-                        if (colWin(grid[c][2], 2, grid)!= ' '){ //if column has possible win
-                            if (colWin(grid[c][2], 2, grid) == 'X'){ // if the column has possible win for X
+                    if (grid[2][c] != ' '){
+                        if (colWin(grid[2][c], 2, grid)!= ' '){ //if column has possible win
+                            if (colWin(grid[2][c], 2, grid) == 'X'){ // if the column has possible win for X
                                 x_wins++;
                                 } 
-                            else if (colWin(grid[c][2], 2, grid) == 'O'){ // if column has possible win for O
+                            else if (colWin(grid[2][c], 2, grid) == 'O'){ // if column has possible win for O
                                 o_wins++;
                             }
                         }     
@@ -75,7 +71,7 @@ public class MiniMax {
                 } // check for third column
                  
                 for (int r = 0; r < 3; r++) {
-                    if (grid[r][0] != ' ' || grid[r][1] != ' ' || grid[r][2] != ' '){ 
+                    if (grid[r][0] != ' '){ 
                         if (rowWin(grid[r][0], r, grid) != ' ') {
                             if (rowWin(grid[r][0], r, grid) == 'X') {
                                 x_wins++;
@@ -87,7 +83,7 @@ public class MiniMax {
                 }
 
                 for (int r = 0; r < 3; r++) {
-                    if (grid[r][0] != ' ' || grid[r][1] != ' ' || grid[r][2] != ' '){ 
+                    if (grid[r][1] != ' '){ 
                         if (rowWin(grid[r][1], r, grid) != ' ') {
                             if (rowWin(grid[r][1], r, grid) == 'X') {
                                 x_wins++;
@@ -99,7 +95,7 @@ public class MiniMax {
                 }
 
                 for (int r = 0; r < 3; r++) {
-                    if (grid[r][0] != ' ' || grid[r][1] != ' ' || grid[r][2] != ' '){ 
+                    if (grid[r][2] != ' '){ 
                         if (rowWin(grid[r][2], r, grid) != ' ') {
                             if (rowWin(grid[r][2], r, grid) == 'X') {
                                 x_wins++;
@@ -139,30 +135,26 @@ public class MiniMax {
                         
                     }
                 }
-                if (x_wins == 0){
-                    if (o_wins == 0){
-                        ratio[i][j] = (float) -30;
-                    }
-                    else{
-                        ratio[i][j] = (float) 30;
-                    }
-                }
-                else {
-                    ratio[i][j] = (float) o_wins - x_wins;
-                }
                 if (sb.isEmpty()){
                     ratio[i][j] = (float) 0; 
                     break; //neutral position, but change to match halfway of top and bottom x ratio
                 }
-                if (sb.getWinner() == 'X'){
-                    ratio[i][j] = (float) 30;
+                else if (sb.getWinner() == 'X'){ //if board is already WON
+                    ratio[i][j] = (float) -4;
                 } else if (sb.getWinner() == 'O'){
-                    ratio[i][j] = (float) -30;
+                    ratio[i][j] = (float) 4;
                 }
-                else if (sb.isPlayable() == false && sb.getWinner() == ' '){
-                    ratio[i][j] = (float) -15; //if the board is full and no winner, neutral position
+                else if (sb.isPlayable() == false && sb.getWinner() == ' '){ // board is tied.
+                    ratio[i][j] = (float) 0; //if the board is full and no winner, neutral position
                 }
 
+                
+                else {
+                    ratio[i][j] = (float) o_wins - x_wins;
+                }
+                
+            
+               
                 o_wins = 0;
                 x_wins = 0;
                 
@@ -189,35 +181,31 @@ public class MiniMax {
         float ratio9 = Math.max(colSum2(ratios, 2), diagSumA(ratios));
         float ratio10 = Math.max(diagSumB(ratios), ratio6);
 
-        return(Math.max(ratio5, Math.max(ratio2, Math.max(ratio3, ratio4))/(Math.max(ratio10, Math.max(ratio7, Math.max(ratio8, ratio9))))));
+        return(Math.max(ratio5, Math.max(ratio2, Math.max(ratio3, ratio4))-(Math.max(ratio10, Math.max(ratio7, Math.max(ratio8, ratio9))))));
         
         
     }
 // player X and player O evals
     public float diagSum1(float[][]ratios){return ratios[0][0] + ratios[1][1] + ratios[2][2];}
-    public float diagSumA(float[][]ratios){return 1/ratios[0][0] + 1/ratios[1][1] + 1/ratios[2][2];} 
-    public float diagSumB(float[][]ratios){return 1/ratios[0][2] + 1/ratios[1][1] + 1/ratios[2][0];}
+    public float diagSumA(float[][]ratios){return -ratios[0][0] - ratios[1][1] - ratios[2][2];} 
+    public float diagSumB(float[][]ratios){return -ratios[0][2] - ratios[1][1] - ratios[2][0];}
     public float diagSum2(float[][]ratios){return ratios[0][2] + ratios[1][1] + ratios[2][0];}
     public float rowSum(float[][]ratios, int row){return ratios[row][0] + ratios[row][1] + ratios[row][2];}
-    public float rowSum2(float[][]ratios, int row){return 1/ratios[row][0] + 1/ratios[row][1] + 1/ratios[row][2];}
-    public float colSum2(float[][]ratios, int col){return 1/ratios[0][col] + 1/ratios[1][col] + 1/ratios[2][col];}
+    public float rowSum2(float[][]ratios, int row){return -ratios[row][0] - ratios[row][1] - ratios[row][2];}
+    public float colSum2(float[][]ratios, int col){return -ratios[0][col] - ratios[1][col] - ratios[2][col];}
     public float colSum(float[][]ratios, int col){return ratios[0][col] + ratios[1][col] + ratios[2][col];}
 
     public char colWin(char val, int col, char[][] grid){
         if (val == 'X'){
             if (grid[0][col] != 'O' && grid[1][col] != 'O' && grid[2][col] != 'O'){
-                if (grid[0][col] != ' ' || grid[1][col] != ' ' || grid[2][col] != ' '){
-                    return 'X';
-                }
+                return 'X';
             }
 
         }
 //DEBUG: column up the middle not checking.
         else if (val == 'O'){
             if (grid[0][col] != 'X' && grid[1][col] != 'X' && grid[2][col] != 'X'){
-                if (grid[0][col] != ' ' || grid[1][col] != ' ' || grid[2][col] != ' '){
-                    return 'O';
-                }
+                return 'O';
             }
         }
         return ' ';
@@ -226,16 +214,12 @@ public class MiniMax {
     public char rowWin(char val, int row, char[][] grid){
         if (val == 'X'){
             if (grid[row][0] != 'O' && grid[row][1] != 'O' && grid[row][2] != 'O'){
-                if (grid[row][0] != ' ' || grid[row][1] != ' ' || grid[row][2] != ' '){
-                    return 'X';
-                }
+                return 'X';
             }
 
         } else if (val == 'O'){
             if (grid[row][0] != 'X' && grid[row][1] != 'X' && grid[row][2] != 'X'){
-                if (grid[row][0] != ' ' || grid[row][1] != ' ' || grid[row][2] != ' '){
-                    return 'O';
-                }
+                return 'O';
             }
         }
         return ' ';
@@ -243,7 +227,7 @@ public class MiniMax {
     }
 
     
-    public float miniMax(BigBoard board, int depth, int player, boolean isMaximizing, float alpha, float beta){
+    public float miniMax(BigBoard board, int depth, boolean isMaximizing){//, float alpha, float beta){
         ArrayList<int[]> legalMoves = board.getAvailableMoves();
         board.checkOverallWinner();
         if (depth == 0 || board.getWinner() != ' ' || legalMoves.size() == 0) {
@@ -253,16 +237,14 @@ public class MiniMax {
             float highestVal = -99999999;
             for (int[] move : legalMoves) {
                 BigBoard boardCopy = deepCopy(board); 
-                if (player % 2 == 0){boardCopy.makeMove(move[0], move[1], move[2], move[3], 'X');} //if player is even, X plays
-                else{boardCopy.makeMove(move[0], move[1], move[2], move[3], 'O');}
+                boardCopy.makeMove(move[0], move[1], move[2], move[3], 'O');
                 
-                float value = miniMax(boardCopy, depth - 1, player+1, false, alpha, beta);
+                float value = miniMax(boardCopy, depth - 1, false);//, alpha, beta);
                 highestVal = Math.max(highestVal, value); //max to maximize the value score,
-                alpha = Math.max(alpha, value);
-                if (beta <= alpha) {
-                    break; // Beta is the lowest other branch move, the non maximizing always chooses lowest
+                //alpha = Math.max(alpha, value);
+                //if (beta <= alpha) {
+                  //  break; // Beta is the lowest other branch move, the non maximizing always chooses lowest
 
-                }
             }
             return highestVal;
         }
@@ -270,18 +252,17 @@ public class MiniMax {
             float lowestVal = 99999999;
             for (int[] move : legalMoves) {
                 BigBoard boardCopy = deepCopy(board); 
+                boardCopy.makeMove(move[0], move[1], move[2], move[3], 'X'); //if player is not maximizing, X plays
 
-                if (player % 2 == 0){boardCopy.makeMove(move[0], move[1], move[2], move[3], 'X');} //if player is even, X plays
-                else{boardCopy.makeMove(move[0], move[1], move[2], move[3], 'O');}
                 
 
-                float value = miniMax(boardCopy, depth - 1, player+1, true, alpha, beta);
+                float value = miniMax(boardCopy, depth - 1, true);//, alpha, beta);
 
                 lowestVal = Math.min(lowestVal, value);
-                beta = Math.min(beta, value);  
-                if (beta <= alpha) {
-                    break; 
-                }
+                //beta = Math.min(beta, value);  
+                //if (beta <= alpha) {
+                 //   break; 
+               // }
                 //min to minimize the value score,
                 //int [] bestMove1 = {move[0], move[1], move[2], move[3]};
                 //bestMove = bestMove1;
@@ -307,13 +288,12 @@ public class MiniMax {
             BigBoard boardCopy = deepCopy(board); // Assuming you have a makeMove function
     
 
-            float moveValue = miniMax(boardCopy, depth, 0, false, -99999, 99999);    
+            float moveValue = miniMax(boardCopy, depth, false);//, -99999, 99999);    
             if (moveValue > bestVal) {
                 bestVal = moveValue;
                 bestMoveFound = move;
             }
         }
-        this.bestMove = bestMoveFound;
         return bestMoveFound;
     }
 
